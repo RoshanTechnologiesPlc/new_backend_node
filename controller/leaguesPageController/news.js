0d2ff837284e55d478299055d38fd2670fb70bc3
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-02-14 15:46:13 +0300

    Update news.js

--- controller/leaguesPageController/news.js
+++ controller/leaguesPageController/news.js
@@ -1,101 +1,102 @@
 const News = require('../../schemas/news_model');
-const idMatch = require("./wordMatch"); // Ensure this path is correct
+const idMatch = require("./wordMatch");
 
+// Main function to handle the request and respond with the filtered news
 module.exports = async (req, res) => {
-    console.log('League news page is requested...');
     const { leagueId, lang } = req.query;
-    const leagueName = idMatch[leagueId]?.[lang];
+    const leagueName = idMatch[leagueId]?.[lang] ?? undefined;
     if (!leagueName) {
-        return res.status(404).json({ message: "League not found" });
+        return res.status(200).json([]);
     }
 
-    const pageNumber = req.query.page ? parseInt(req.query.page) : 0;
+    const pageNumber = req.query.page ? parseInt(req.query.page) : 1;
     const pageSize = 10;
-var language;
-    switch (req.query.lang) {
-      case"en": 
-        language = "en"
-        break; 
-      case "am":
-        language = "am"
-        break
-      case "tr" : 
-        language = "ti"
-        break;
-      case "or" : 
-        language = "or"
-        break;
-      case "si" :
-        language = "so"
-          // default "Amharic":
-        break;
 
-    try {
-        const newsList = await findNewsForLeague(lang, pageNumber, pageSize, leagueName);
+    const newsList = await findNewsForLeague(pageNumber, pageSize, leagueName, lang);
+
+    if (newsList) {
         return res.status(200).json(newsList);
-    } catch (error) {
-        console.error(`Error fetching news for league: ${error}`);
-        return res.status(500).json({ message: "Internal server error" });
+    } else {
+        return res.status(502).json({ message: "something went wrong" });
     }
 };
 
-async function findNewsForLeague(lang, pageNumber, pageSize, leagueName) {
-    const escapeRegExp = text => text.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&');
-    const regexPattern = new RegExp(escapeRegExp(leagueName), 'i');
-
-    const aggregationPipeline = [
-        {
-            $match: {
-                $or: [
-                    { title: regexPattern },
-                    { description: regexPattern },
-                    { figCaption: regexPattern },
-                    { figCaption_am: regexPattern },
-                    { figCaption_or: regexPattern },
-                    { figCaption_so: regexPattern },
-                    { figCaption_ti: regexPattern },
-                    {summarized: regexPattern},
-                    {summarized_am: regexPattern},
-                    {summarized_or: regexPattern},
-                    {summarized_so: regexPattern},
-                    {summarized_ti: regexPattern},
-                    {summarizedDescription: regexPattern},
-                    {summarizedDescription_am: regexPattern},
-                    {summarizedDescription_or: regexPattern},
-                    {summarizedDescription_so: regexPattern},
-                    {summarizedDescription_ti: regexPattern},
-                    {summarizedTitle: regexPattern},
-                    {summarizedTitle_am: regexPattern},
-                    {summarizedTitle_or: regexPattern},
-                    {summarizedTitle_so: regexPattern},
-                    {summarizedTitle_ti: regexPattern},
-
-
-
-                    // Add other fields where leagueName might appear
-                ]
-            }
-        },
-        { $sort: { publishedDate: -1, createdAt: -1 } },
-        { $skip: pageNumber * pageSize },
-        { $limit: pageSize },
-        {
-            $project: {
-                title: 1,
-                description: 1,
-                mainImage: 1,
-                publishedDate: 1,
-                // Include other fields as necessary
-            }
-        }
-    ];
-
+// Function to find news for a league based on language, page number, page size, and league name
+async function findNewsForLeague(pageNumber, pageSize, leagueName, lang) {
     try {
+        pageNumber = pageNumber - 1; // Adjust for zero-based index
+
+        // Determine the appropriate field names based on the language code
+        let titleField, summarizedField;
+        switch (lang) {
+            case 'am':
+                titleField = 'summarizedTitle_am';
+                summarizedField = 'summarized_am';
+                break;
+            case 'or':
+                titleField = 'summarizedTitle_or';
+                summarizedField = 'summarized_or';
+                break;
+            case 'tr':
+                titleField = 'summarizedTitle_tg';
+                summarizedField = 'summarized_tg';
+                break;
+            case 'so':
+                titleField = 'summarizedTitle_so';
+                summarizedField = 'summarized_so';
+                break;
+            default:
+                // Assuming default to English if language code doesn't match
+                titleField = 'title'; // English uses the 'title' field without suffix
+                summarizedField = 'summarized'; // Default summarized field if exists or adjust as needed
+                break;
+        }
+
+        const aggregationPipeline = [
+            {
+                $project: {
+                    [titleField]: 1,
+                    [summarizedField]: 1,
+                    mainImage: 1,
+                    publishedDate: 1,
+                    occurrenceCount: {
+                        $sum: [
+                            {
+                                $cond: [
+                                    { $regexMatch: { input: `$${titleField}`, regex: new RegExp(escapeRegex(leagueName), 'i') } },
+                                    1,
+                                    0
+                                ]
+                            },
+                            {
+                                $cond: [
+                                    { $regexMatch: { input: `$${summarizedField}`, regex: new RegExp(escapeRegex(leagueName), 'i') } },
+                                    1,
+                                    0
+                                ]
+                            }
+                        ]
+                    }
+                }
+            },
+            { $match: { occurrenceCount: { $gte: 1 } } },
+            { $skip: pageNumber * pageSize },
+            { $limit: pageSize },
+            { $sort: { publishedDate: -1 } }
+        ];
+
         const newsArticles = await News.aggregate(aggregationPipeline);
-        console.log(`Fetched ${newsArticles.length} news articles for league.`);
         return newsArticles;
-    } catch (error) {
-        console.error(`Error in aggregation for league news: ${error}`);
-        throw error; // Rethrow to be caught by the caller
+    } catch (e) {
+        console.log(`Error happened in findNewsForLeague ${e}`);
+        return false;
     }
-};
+}
+
+// Helper function to escape regex special characters in team names
+function escapeRegex(text) {
+    if (typeof text !== 'string' || !text) {
+      return ''; // Return an empty string if text is undefined or not a string
+    }
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
