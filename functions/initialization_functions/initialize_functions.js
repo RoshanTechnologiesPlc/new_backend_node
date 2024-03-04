0f50eba8e353f9dc34e702e619b53754bed946fa
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-03-04 20:12:31 +0300

    Update remover from return  initialize_functions.js

--- functions/initialization_functions/initialize_functions.js
+++ functions/initialization_functions/initialize_functions.js
@@ -1,12 +1,9 @@
-const updateHighlights = require('./fetch_highlights')
+
 const updateTeamStat = require('./update_team_stat')
 const updatePlayerStat = require('./update_player_stat')
 const updateMatches = require('./update_matches')
 const updateStandings = require('./update_standings')
-const updateNews = require('./update_news')
 async function updateData(){
-    return console.log("update data noop")
-    await updateHighlights()
     await updateTeamStat()
     await updatePlayerStat()
     await updateMatches()
