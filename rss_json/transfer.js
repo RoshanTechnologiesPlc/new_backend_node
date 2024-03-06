900f2916a39ba39af729c02f42db5154c6ca2910
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-03-06 18:11:13 +0300

    Update transfer scrapping transfer.js

--- rss_json/transfer.js
+++ rss_json/transfer.js
@@ -1,131 +1,129 @@
-const axios = require("axios");
-const transliteratePlayers = require("../fetch/player_transliteration");
-const parseString = require("xml2js").parseString;
-const util = require("util");
-const Transfer = require("../schemas/transfer");
-const parseStringPromise = util.promisify(parseString);
-const mongoose = require ("mongoose");
-const url = 'mongodb+srv://abubekersiraj:Mongodbpassword1234@test.zezynu2.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(url).then(() => {
- // console.log("Connected to the database");
-  getTransferRssFeed() 
+const axios = require("axios"); 
+const transliteratePlayers = require("../fetch/player_transliteration");  
+const Transfer = require("../schemas/transfer"); 
+const mongoose = require ("mongoose"); 
+const url = 'mongodb+srv://abubekersiraj:Mongodbpassword1234@test.zezynu2.mongodb.net/?retryWrites=true&w=majority'; 
+const cheerio = require('cheerio'); 
+const axiosRetry = require('axios-retry');
+
+
+
+mongoose.connect(url).then(() => { 
+ console.log("Connected to the database"); 
+ savePlayersDataToMongo();
+} 
+).catch((error) => { 
+  console.log("Error connecting to the database", error.message); 
+} 
+); 
+const formatText = (text) => text ? text.replace(/[\n\r\t]+/g, ' ').trim() : ''; 
+const formatCurrency = (value) => { 
+    let result = value.replace(/[€\?,-]/g, '').trim(); 
+    if (result.endsWith('m')) { 
+        // Convert the string to a float, remove the 'm', and then append 'M€' 
+        // The parseFloat will remove unnecessary trailing zeros 
+        return `${parseFloat(result.replace('m', ''))}M€`; 
+    } else if (result.endsWith('Th')) { 
+        // Convert thousands to a fraction of a million for consistency 
+        // This keeps the conversion accurate and appends 'M€' 
+        return `${parseFloat(result.replace('Th', '')) / 1000}M€`; 
+    } 
+    return parseFloat(result) || "0M€"; // Ensure even zero values are represented consistently 
+}; 
+async function isPlayerNameExists(englishNamePlayer) {
+  const playerExists = await Transfer.findOne({"playerName.EnglishName": englishNamePlayer});
+  return !!playerExists; // Returns true if the player exists, false otherwise
 }
-).catch((error) => {
-  console.log("Error connecting to the database", error.message);
-}
-);
+ 
+const getPlayerData = async (elem, $) => { 
+    // Assume 'elem' is an element in the page corresponding to a player 
+    const cells = $(elem).find('td');
+    const englishNamePlayer = formatText($(cells[1]).find('img').attr('title'));
 
-
-
-
-
-
-const rssUrl = "https://crssnt.com/preview?id=1h-XoXOlAeY-ZriweURBNqf7SnQfmejrI799RxvWlYpg&name=big";
-//  const rssUrl =
-  //  "https://crssnt.com/preview/https:/docs.google.com/spreadsheets/d/1HinQ1XEghgtuqlGz9GKa9Hw_DF8bSjQ-pv4MPuihZAg";
-
-async function getTransferRssFeed() {
-  try {
-    const response = await axios.get(rssUrl);
-
-
-    if (response.status === 200) {
-      const xmlData = response.data;
-      const result = await parseStringPromise(xmlData, { explicitArray: false });
-      const items = result.rss.channel.item.map((node) => ({
-        fromClubName: extractFromClub(node.title),
-        toClubName: extractToClub(node.title),
-        playerName: extractName(node.title),
-        transferAmount: extractTransferAmount(node.title),
-        playerProfile: constructPhotoUrl(extractName(node.title), 'player'),
-        fromClubPhoto: constructPhotoUrl(extractFromClub(node.title), 'club'),
-        toClubPhoto: constructPhotoUrl(extractToClub(node.title), 'club'),
-      }));
-
-      for (const item of items) {
-        const response = await fetchDataByPlayerName(item.playerName);
-       // console.log("players data :-", item.playerName);
-      
-        if (!response) {
-          const result = {
-            fromClubName: await transliteratePlayers(item.fromClubName),
-            
-            toClubName: await transliteratePlayers(item.toClubName),
-            playerName: await transliteratePlayers(item.playerName),
-            transferAmount: item.transferAmount,
-            playerProfile: item.playerProfile,
-            fromClubPhoto: item.fromClubPhoto,
-            toClubPhoto: item.toClubPhoto,
-          };
-          await Transfer.create(result);
-          console.log("Inserted new player data:", result);
-
-        } else {
-          //console.log("Player found:", response);
-        }
-      }
-      
-      module.exports = Transfer;
-    } else {
-      console.log(`HTTP error ${response.status}`);
-      return [];
+    if (await isPlayerNameExists(englishNamePlayer)) {
+      console.log(`Skipping ${englishNamePlayer}, already exists.`);
+      return null; // Return null to indicate that no new data should be processed for this player
     }
-  } catch (e) {
-    console.error(`Error: ${e}`);
-    return [];
-  }
-}
 
-function extractName(name) {
-  const match = name.match(/^([^\(]+)/);
-  return match ? match[0].trim() : "";
-}
+    const englishNameFromClub = formatText($(cells[9]).find('img').attr('title')  || 'Without club'); 
+    const englishNameToClub = formatText($(cells[13]).find('img').attr('title') || 'Without club'); 
+    
+    // Perform transliteration for playerName 
+    const playerNametransliterate = await transliteratePlayers(englishNamePlayer); 
 
-function extractFromClub(fromClubName) {
-  const regex = /\(([^)]+)\➡️/;
-  const match = fromClubName.match(regex);
-  return match && match[1];
-}
+     
+    const fromClubNametransliterate = await transliteratePlayers(englishNameFromClub); 
 
-function extractToClub(toClubName) {
-  const regex = /➡️\s([^💲:]+)/;
-  const match = toClubName.match(regex);
-  return match && match[1];
-}
+ 
+    // Perform transliteration for toClubName 
+    const toClubNametransliterate = await transliteratePlayers(englishNameToClub); 
 
-function extractTransferAmount(transferAmount) {
-  const regex = /: [^]+/;
-  const match = transferAmount.match(regex);
-  let extractedTransferInfo = match ? match[0].trim() : "Transfer info not found";
-  // Translation and special cases handling here
-  // ...
-  return extractedTransferInfo;
-}
+   
+     
+    const playerData = { 
+    
+        fromClubName: { 
+            EnglishName: englishNameFromClub, 
+            AmharicName: fromClubNametransliterate.AmharicName, 
+            OromoName: fromClubNametransliterate.OromoName, 
+            SomaliName: fromClubNametransliterate.SomaliName, 
+        }, 
+        toClubName: { 
+            EnglishName: englishNameToClub, 
+            AmharicName: toClubNametransliterate.AmharicName, 
+            OromoName: toClubNametransliterate.OromoName, 
+            SomaliName: toClubNametransliterate.SomaliName, 
+        }, 
+        playerProfile: ($(cells[1]).find('img').attr('data-src') || "https://www.pngfind.com/pngs/m/622-6227463_man-head-people-avatar-svg-png-icon-free.png"), 
+        fromClubPhoto: ($(cells[9]).find('img').attr('src') || "No Photo").replace('/tiny/', '/big/'), 
+        toClubPhoto: ($(cells[13]).find('img').attr('src') || "No Photo").replace('/tiny/', '/big/'), 
+        playerName: { 
+            EnglishName: englishNamePlayer, 
+            AmharicName: playerNametransliterate.AmharicName, 
+            OromoName: playerNametransliterate.OromoName, 
+            SomaliName: playerNametransliterate.SomaliName, 
+        }, 
+        transferAmount: formatCurrency($(cells[16]).text()), 
+    }; 
+    return playerData; 
+}; 
 
-function constructPhotoUrl(name, type) {
-  if (name == null) {
-    return "";
-  }
-  name = name.replace(/\.$/, "").replace(/\s+/g, "-");
-  const normalizedString = name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  const lowercaseName = normalizedString.toLowerCase();
+const getPlayersData = async (pages) => { 
+    let playersData = []; 
+    for (let page = 1; page <= pages; page++) {
 
-  if (type === 'player') {
-    return `https://fifaratings.com/wp-content/uploads/${lowercaseName}.png`;
-  } else if (type === 'club') {
-    return `https://fifaratings.com/wp-content/uploads/${lowercaseName}.png`;
-  }
-  return "";
-}
-
-async function fetchDataByPlayerName(playerName) {
-  try {
-    return await Transfer.findOne({ "playerName.EnglishName": playerName });
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-  }
-}
-
-module.exports = getTransferRssFeed;
+const pageUrl = 'https://www.transfermarkt.com/transfers/saisontransfers/statistik/top/plus/1/galerie/0?saison_id=2023&transferfenster=alle&land_id=&ausrichtung=&spielerposition_id=&altersklasse=&leihe= '
+const headers = { 
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36" 
+        }; 
+        const response = await axios.get(pageUrl, { headers }); 
+        const $ = cheerio.load(response.data); 
+        const pagePromises = []; 
+ 
+        $('.responsive-table .grid-view .items tbody tr').each((_, elem) => { 
+            pagePromises.push(getPlayerData(elem, $)); 
+        }); 
+ 
+        // Wait for all getPlayerData promises of this page to resolve 
+        const results = await Promise.all(pagePromises); 
+        playersData = playersData.concat(results.filter(player => player.playerName.EnglishName)); // Collect and filter data from this page 
+    } 
+    return playersData; // Contains all players' data from all pages 
+}; 
+ 
+const savePlayersDataToMongo = async () => { 
+    try { 
+      const playersData = await getPlayersData(1); // Fetch the player data
+      let lastIndex = await Transfer.find().sort({index: -1}).limit(1).then(docs => docs[0] ? docs[0].index : 0); // Find the last index used
+      for (let playerData of playersData) {
+        lastIndex++; // Increment the index for each new document
+        playerData.index = lastIndex; // Assign the incremented index to the new document
+        await Transfer.create(playerData); 
+        console.log('inserted this players data ')
+      }
+    } catch (error) { 
+        console.error('Failed to save player data to MongoDB:', error); 
+    } 
+}; 
 
