235c176edf1757089c77c14c4a61e707ed8c661c
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-03-06 19:35:18 +0300

    Update doesnt fetch null values of amharic name  transferController.js

--- controller/transfer/transferController.js
+++ controller/transfer/transferController.js
@@ -3,27 +3,37 @@ const getTransferRssFeed = require("../../rss_json/transfer");
 const transliteratePlayers = require("../../fetch/player_transliteration");
 // fetch all data desc order
 const index = (req, res) => {
- 
-  try{
- 
-    const pageNumber= +parseInt(req.query.pageNumber)
-    const pageSize = 10;
-    Transfer.find()
-      .sort({ createdAt: 1 }).skip((pageNumber - 1) * pageSize).limit(pageSize)
+  try {
+    const pageNumber = +parseInt(req.query.pageNumber);
+    const pageSize = 3;
+    // Query to ensure none of the AmharicName fields are missing or empty
+    const amharicNameExistsQuery = {
+      $and: [
+        { "fromClubName.AmharicName": { $exists: true, $ne: "" } },
+        { "toClubName.AmharicName": { $exists: true, $ne: "" } },
+        { "playerName.AmharicName": { $exists: true, $ne: "" } },
+      ],
+    };
+
+    Transfer.find(amharicNameExistsQuery)
+      .sort({ createdAt: 1 })
+      .skip((pageNumber - 1) * pageSize)
+      .limit(pageSize)
       .then((response) => {
         res.status(200).json({
           response,
         });
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
         res.status(502).json({
           message: "An error Occured",
         });
       });
-  }catch(e){
-    res.status(404).json({message : "error"})
+  } catch (e) {
+    res.status(404).json({ message: "error" });
   }
 };
+
 
 module.exports =  index
