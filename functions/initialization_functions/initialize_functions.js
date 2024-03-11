132970dcf1635aa78a1808e944cc26238c0e5b96
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-03-11 11:59:13 +0300

    Update comented  initialize_functions.js

--- functions/initialization_functions/initialize_functions.js
+++ functions/initialization_functions/initialize_functions.js
@@ -1,14 +1,14 @@
 
-const updateTeamStat = require('./update_team_stat')
-const updatePlayerStat = require('./update_player_stat')
-const updateMatches = require('./update_matches')
-const updateStandings = require('./update_standings')
-async function updateData(){
-    await updateTeamStat()
-    await updatePlayerStat()
-    await updateMatches()
-    await updateStandings()
-    await updateNews()
-}
+// const updateTeamStat = require('./update_team_stat')
+// const updatePlayerStat = require('./update_player_stat')
+// const updateMatches = require('./update_matches')
+// const updateStandings = require('./update_standings')
+// async function updateData(){
+//     await updateTeamStat()
+//     await updatePlayerStat()
+//     await updateMatches()
+//     await updateStandings()
+//     await updateNews()
+// }
 
-module.exports = updateData
+// module.exports = updateData
