726d90718342364b46056c9b93800839de71119a
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-03-16 18:15:50 +0300

    Update isPodcastLive.js

--- functions/isPodcastLive.js
+++ functions/isPodcastLive.js
@@ -3,32 +3,22 @@ function isPodcastCurrentlyLive(podcast) {
 
     try {
         var currentDate = new Date();
-
-        // Get the day of the week as a number (Sunday is 0, Monday is 1, ..., Saturday is 6)
         var dayOfWeek = currentDate.getDay();
+        dayOfWeek = (dayOfWeek + 6) % 7; // Adjust day of the week
         
-        // Adjust the result to have Monday as 0 and Sunday as 6
-        dayOfWeek = (dayOfWeek + 6) % 7;
         let time = podcast.liveTimes[dayOfWeek];
+        if (!time) { // Check if time is undefined or null
+            console.error("No live time scheduled for today.");
+            return false;
+        }
 
-        const startTime = new Date();
-        startTime.setFullYear(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
-        startTime.setHours(time['start-hour'], time['start-minute'], 0, 0);
-        const endTime = new Date();
-        endTime.setFullYear(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
-        endTime.setHours(time['end-hour'], time['end-minute'], 0, 0);
+        const startTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), time['start-hour'], time['start-minute'], 0);
+        const endTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), time['end-hour'], time['end-minute'], 0);
 
-        const isLive = now >= startTime && now <= endTime;
-        return isLive;
+        return now >= startTime && now <= endTime;
       
     } catch (e) {
-        // Handle any errors, possibly by logging or returning a default value
         console.error("Error in isPodcastCurrentlyLive: ", e);
         return false;
     }
 }
-
-module.exports = isPodcastCurrentlyLive;
-
-
-module.exports = isPodcastCurrentlyLive;
