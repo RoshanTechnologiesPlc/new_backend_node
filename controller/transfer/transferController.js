a1ae52c30a8fbe68598d20c3a191641b041cdab4
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-03-16 18:11:10 +0300

    Update transferController.js

--- controller/transfer/transferController.js
+++ controller/transfer/transferController.js
@@ -1,6 +1,6 @@
 const Transfer = require("../../schemas/transfer");
 
-// This version removes pagination capability by not skipping any documents.
+// This version reverses the sort order of the documents.
 const index = (req, res) => {
   const handleError = (error, message = "An error occurred") => {
     console.log(error);
@@ -15,7 +15,7 @@ const index = (req, res) => {
     const pageSize = 3; // Controls the number of documents returned
 
     Transfer.find({})
-      .sort({ createdAt: -1 }) // Sorts documents by creation time in descending order
+      .sort({ createdAt: 1 }) // Sorts documents by creation time in ascending order
       .limit(pageSize) // Limits the number of documents to pageSize
       .then(sendResponse) // Sends the retrieved documents as a response
       .catch((error) => handleError(error, "An error Occurred"));
