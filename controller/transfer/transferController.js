7218b9eada5158b554a26afa0c9ad56b765b8cbf
RoshanTechnologiesPlc <154740300+RoshanTechnologiesPlc@users.noreply.github.com>
2024-03-16 17:02:42 +0300

    update upside down transferController.js

--- controller/transfer/transferController.js
+++ controller/transfer/transferController.js
@@ -2,19 +2,25 @@ const Transfer = require("../../schemas/transfer");
 const getTransferRssFeed = require("../../rss_json/transfer");
 const transliteratePlayers = require("../../fetch/player_transliteration");
 
-// fetch all data desc order
+// Modified index function
 const index = (req, res) => {
   try {
     const pageNumber = +parseInt(req.query.pageNumber);
     const pageSize = 3;
 
+    // Fetch data in ascending order this time
     Transfer.find({})
-      .sort({ createdAt: -1 }) // Changed to descending order
+      .sort({ createdAt: 1 }) // Ascending order
       .skip((pageNumber - 1) * pageSize)
       .limit(pageSize)
       .then((response) => {
+        // Process data to exclude the 2nd item from the end after reversing
+        const processedResponse = response
+          // .filter((_, index) => index !== 1) // Skip the 2nd item (since we will reverse the array, it corresponds to the second-to-last item in original order)
+          .reverse(); // Reverse to make it upside down
+
         res.status(200).json({
-          response,
+          response: processedResponse,
         });
       })
       .catch((error) => {
